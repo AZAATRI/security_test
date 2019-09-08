5e3accc4c3fa4c3527044e074a8cfa831ca828db
azaatri <amine.zaatri@gmail.com>
2019-09-08 15:40:23 +0200

    test 1

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Hslavich\OneloginSamlBundle\Security\Authentication\Token\SamlTokenFactory;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class LoginController extends AbstractController
+{
+    private $authenticationManager;
+    private $tokenStorage;
+    public function __construct(TokenStorageInterface $tokenStorage,AuthenticationManagerInterface $authenticationManager)
+    {
+        $this->authenticationManager = $authenticationManager;
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function index()
+    {
+        $tokenFactory = new SamlTokenFactory();
+        $token = $tokenFactory->createToken(new User(),array(),[]);
+        $token = $this->authenticationManager->authenticate($token);
+        $this->tokenStorage->setToken($token);
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+        ]);
+    }
+    /**
+     * @Route("/checkd", name="check_it")
+     */
+    public function checker()
+    {
+        die();
+        $tokenFactory = new SamlTokenFactory();
+        $token = $tokenFactory->createToken(new User(),array(),[]);
+        $token = $this->authenticationManager->authenticate($token);
+        $this->tokenStorage->setToken(null);
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+    /**
+     * @Route("/logoutf", name="logout")
+     */
+    public function logout()
+    {
+        $tokenFactory = new SamlTokenFactory();
+        $token = $tokenFactory->createToken(new User(),array(),[]);
+        $token = $this->authenticationManager->authenticate($token);
+        $this->tokenStorage->setToken(null);
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Entity;
+
+use Hslavich\OneloginSamlBundle\Security\User\SamlUserInterface;
+
+class User implements SamlUserInterface
+{
+    protected $username;
+    private $roles = [];
+    // ...
+
+    public function __construct()
+    {
+        $this->roles = ['ROLE_USER'];
+    }
+
+    public function setSamlAttributes(array $attributes)
+    {
+        //$this->email = $attributes['mail'][0];
+    }
+
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    public function getPassword()
+    {
+        // TODO: Implement getPassword() method.
+    }
+
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    public function getUsername()
+    {
+        return "aa";
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+}
